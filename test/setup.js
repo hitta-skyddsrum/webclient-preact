512b20d187a305c53ce3a15f57b7cb03aec8ff20
kontrollanten <alexander@width.se>
2017-10-28 00:40:36 +0200

    add sinon chai support in test setup

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime/runtime';
 import chai from 'chai';
+import sinonChai from 'sinon-chai';
 import assertJsx, { options } from 'preact-jsx-chai';
 
 // when checking VDOM assertions, don't compare functions, just nodes and attributes:
@@ -8,4 +9,6 @@ options.functions = false;
 // activate the JSX assertion extension:
 chai.use(assertJsx);
 
-global.sleep = ms => new Promise( resolve => setTimeout(resolve, ms) );
+chai.use(sinonChai);
+
+global.sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
