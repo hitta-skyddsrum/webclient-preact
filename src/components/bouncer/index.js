415ff8777b21a231f9d2d3ad462b7cd7a93dffaa
kontrollanten <alexander@width.se>
2018-01-01 09:04:39 +0000

    bouncer: add tests and cleanup

--- src/components/bouncer/index.js
+++ src/components/bouncer/index.js
@@ -2,17 +2,18 @@ import { h } from 'preact';
 import Classnames from 'classnames';
 import style from './style.scss';
 
-export default () => {
+export default ({ position }) => {
   const cx = Classnames.bind(style);
   const className = cx([
-    'bouncer',
+    style.bouncer,
+    style[position],
   ]);
 
   return (
     <div class={className}>
-      <div class="bounce1" />
-      <div class="bounce2" />
-      <div class="bounce3" />
+      <div />
+      <div class={style.bounce2} />
+      <div />
     </div>
   );
 };
---
+++ src/components/bouncer/index.test.js
@@ -0,0 +1,14 @@
+import { h } from 'preact';
+import { shallow } from 'preact-render-spy';
+import { expect } from 'chai';
+
+import Bouncer from './';
+
+describe('components/bouncer', () => {
+  it('should display a div with three sub divs', () => {
+    const elem = shallow(<Bouncer />);
+
+    expect(elem.find('div').length).to.equal(4);
+    expect(elem.find('div').find('div').length).to.equal(3);
+  });
+});
