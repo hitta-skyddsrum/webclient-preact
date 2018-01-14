3f0c138842a194668cef6c1fae5f856336aa9af0
kontrollanten <alexander@width.se>
2018-01-14 11:17:16 +0000

    e2e/home: save visual regressions with window size based filename

--- test/e2e/home.e2e.js
+++ test/e2e/home.e2e.js
@@ -1,17 +1,23 @@
 module.exports = {
   'Home' (browser) {
+    const windowSize = browser.options.desiredCapabilities.chromeOptions.args
+      .filter(arg => arg.indexOf('window-size') !== -1)
+      .map(arg => arg.replace('window-size=', ''))
+      .map(size => size.replace(',', 'x'))
+      .pop();
+
     browser
       .url(browser.launchUrl)
       .waitForElementVisible('body', 1000)
       .assert.title('Hitta skyddsrum')
       .waitForElementVisible('input[type=text]', 3000)
-      .compareScreenshot('home-step-1.png')
+      .compareScreenshot(`home-step-1-${windowSize}.png`)
       .setValue('input[type=text]', 'Stockholmsvägen')
       .waitForElementVisible('ul li:first-child', 1000)
-      .compareScreenshot('home-step-2.png')
+      .compareScreenshot(`home-step-2-${windowSize}.png`)
       .click('ul li:nth-child(2)')
       .waitForElementVisible('.leaflet-container', 1500)
-      .compareScreenshot('home-step-3.png')
+      .compareScreenshot(`home-step-3-${windowSize}.png`)
       .assert.urlContains('lat=')
       .pause(1000)
       .end();
