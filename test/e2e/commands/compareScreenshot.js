898e46b93d06080f0abd65c5f000d978bdfa87b6
kontrollanten <alexander@width.se>
2018-01-14 11:50:10 +0000

    e2e: name visual regression files by action

--- test/e2e/commands/compareScreenshot.js
+++ test/e2e/commands/compareScreenshot.js
@@ -1,15 +1,18 @@
 // Based on https://gist.github.com/richard-flosi/8a5d2e10b6609ab9d06a
 
-exports.command = function(filename, expected, callback) {
+exports.command = function(actionName, expected, callback) {
   const windowSize = this.options.desiredCapabilities.chromeOptions.args
     .filter(arg => arg.indexOf('window-size') !== -1)
     .map(arg => arg.replace('window-size=', ''))
     .map(size => size.replace(',', 'x'))
     .pop();
+  const filename = `${this.currentTest.module}-${this.currentTest.name}-`
+    .concat(actionName.replace(/ /g, '_'))
+    .concat(`-${windowSize}.png`);
 
   const self = this,
     screenshotPath = this.globals.visual_regression.resultPath,
-    resultPath = screenshotPath + '/results/' + filename.replace(/(\.[^.]+)$/, `-${windowSize}$1`);
+    resultPath = screenshotPath + '/results/' + filename;
 
   self.saveScreenshot(resultPath, (result, error) => {
     if (error) {
