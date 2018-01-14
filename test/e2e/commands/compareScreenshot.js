35de27e561f1dc85f84d414a49a28aa2a08e4c40
kontrollanten <alexander@width.se>
2018-01-14 11:25:51 +0000

    e2e visual regression: move window size based filename to cmd

--- test/e2e/commands/compareScreenshot.js
+++ test/e2e/commands/compareScreenshot.js
@@ -1,9 +1,15 @@
 // Based on https://gist.github.com/richard-flosi/8a5d2e10b6609ab9d06a
 
 exports.command = function(filename, expected, callback) {
-  let self = this,
+  const windowSize = this.options.desiredCapabilities.chromeOptions.args
+    .filter(arg => arg.indexOf('window-size') !== -1)
+    .map(arg => arg.replace('window-size=', ''))
+    .map(size => size.replace(',', 'x'))
+    .pop();
+
+  const self = this,
     screenshotPath = this.globals.visual_regression.resultPath,
-    resultPath = screenshotPath + '/results/' + filename;
+    resultPath = screenshotPath + '/results/' + filename.replace(/(\.[^.]+)$/, `-${windowSize}$1`);
 
   self.saveScreenshot(resultPath, (result, error) => {
     if (error) {
--- test/e2e/home.e2e.js
+++ test/e2e/home.e2e.js
@@ -1,23 +1,17 @@
 module.exports = {
   'Home' (browser) {
-    const windowSize = browser.options.desiredCapabilities.chromeOptions.args
-      .filter(arg => arg.indexOf('window-size') !== -1)
-      .map(arg => arg.replace('window-size=', ''))
-      .map(size => size.replace(',', 'x'))
-      .pop();
-
     browser
       .url(browser.launchUrl)
       .waitForElementVisible('body', 1000)
       .assert.title('Hitta skyddsrum')
       .waitForElementVisible('input[type=text]', 3000)
-      .compareScreenshot(`home-step-1-${windowSize}.png`)
+      .compareScreenshot(`home-step-1.png`)
       .setValue('input[type=text]', 'Stockholmsvägen')
       .waitForElementVisible('ul li:first-child', 1000)
-      .compareScreenshot(`home-step-2-${windowSize}.png`)
+      .compareScreenshot(`home-step-2.png`)
       .click('ul li:nth-child(2)')
       .waitForElementVisible('.leaflet-container', 1500)
-      .compareScreenshot(`home-step-3-${windowSize}.png`)
+      .compareScreenshot(`home-step-3.png`)
       .assert.urlContains('lat=')
       .pause(1000)
       .end();
