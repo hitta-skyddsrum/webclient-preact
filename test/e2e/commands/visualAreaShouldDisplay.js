17cc786050af652c6edc7e254f5090cf81059f9f
kontrollanten <alexander@width.se>
2018-02-26 21:00:21 +0000

    e2e: rename baseline images to have browser name at end
    It's easier to compare images between browsers when the
    browser name is appended instead of preprended.

--- test/e2e/commands/visualAreaShouldDisplay.js
+++ test/e2e/commands/visualAreaShouldDisplay.js
@@ -2,9 +2,11 @@
 
 exports.command = function(actionName, expected, callback) {
   const { windowSize } = this.globals.test_settings;
-  const filename = `${this.options.desiredCapabilities.browserName}-${this.currentTest.module}-${this.currentTest.name}-`
+  const filename = `${this.currentTest.module}-${this.currentTest.name}-`
     .concat(actionName.replace(/ /g, '_'))
-    .concat(`-${windowSize.width}x${windowSize.height}.png`);
+    .concat(`-${windowSize.width}x${windowSize.height}`)
+    .concat(`-${this.options.desiredCapabilities.browserName}`)
+    .concat('.png');
 
   const self = this,
     screenshotPath = this.globals.visual_regression.resultPath,
