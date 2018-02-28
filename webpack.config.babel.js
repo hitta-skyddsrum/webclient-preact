2f211a32ef8e114380f764005aff1516faa40b07
kontrollanten <alexander@width.se>
2018-02-28 21:25:46 +0100

    fix MuiThemeProvider
    When uglifying with `unused: true` MuiThemeProvider returns
    its children as an array instead of an VNode, which makes the
    whole element become `undefined`.
    
    https://github.com/mui-org/material-ui/issues/7721#issuecomment-369368001

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,6 +5,7 @@ import autoprefixer from 'autoprefixer';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import path from 'path';
 const ENV = process.env.NODE_ENV || 'development';
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 require('dotenv').config();
 
 const CSS_MAPS = ENV !== 'production';
@@ -36,7 +37,38 @@ module.exports = {
 	},
 
   optimization: {
-    minimize: ENV === 'production',
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false
+          },
+          compress: {
+            unsafe_comps: true,
+            properties: true,
+            keep_fargs: false,
+            pure_getters: true,
+            collapse_vars: true,
+            unsafe: true,
+            ie8: false,
+            warnings: false,
+            sequences: true,
+            dead_code: true,
+            drop_debugger: true,
+            comparisons: true,
+            conditionals: true,
+            evaluate: true,
+            booleans: true,
+            loops: true,
+            unused: false,
+            hoist_funs: true,
+            if_return: true,
+            join_vars: true,
+            drop_console: true
+          }
+        }
+      }),
+    ]
   },
 
 	module: {
