2bc64eed0863312d8b8b4b0142734f85182770b2
kontrollanten <alexander@width.se>
2018-03-03 22:09:54 +0100

    shelters-map: use https for Leaflet tiles

--- src/components/shelters-map/index.js
+++ src/components/shelters-map/index.js
@@ -28,8 +28,8 @@ export default ({
         {...!!bounds.length && { bounds }}
       >
         <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution="&copy; <a href=&quot;https://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
         {!!youAreHere.length && <Marker
           position={youAreHere}
