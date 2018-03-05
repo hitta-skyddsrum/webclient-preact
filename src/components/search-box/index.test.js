4ec440ac0374120d3b8352e0d22c6de6adc4b0bb
kontrollanten <alexander@width.se>
2018-03-05 13:43:03 +0100

    search-box: fix Algolia error handling

--- src/components/search-box/index.test.js
+++ src/components/search-box/index.test.js
@@ -4,44 +4,117 @@ import sinon from 'sinon';
 import { shallow } from 'preact-render-spy';
 import { route } from 'preact-router';
 import AlgoliaPlaces from 'algolia-places-react';
+import ErrorDialog from '../error-dialog';
 import SearchBox from './';
 
 describe('components/search-box', () => {
-  it('should contain AlgoliaPlaces component', () => {
-    const apiKey = 'api-123';
-    const appId = 'app-no-1';
+  describe('ErrorDialog', () => {
+    it('should display an ErrorDialog upon state error truthy', () => {
+      const context = shallow(<SearchBox />);
+      const title = 'This is the title';
+      const desc = 'Descriptionable description';
 
-    process.env.ALGOLIA_API_KEY = apiKey;
-    process.env.ALGOLIA_APP_ID = appId;
+      expect(context.find(<ErrorDialog />).length).to.equal(0);
 
-    expect(<SearchBox />).to.contain(<AlgoliaPlaces
-      placeholder="Var vill du söka från?"
-      options={{
-        language: 'sv',
-        countries: ['se'],
-        apiKey,
-        appId,
-      }}
-    />);
+      context.setState({
+        error: {
+          title,
+          desc,
+        },
+      });
+
+      expect(context.find(<ErrorDialog title={title} desc={desc} />).length).to.equal(1);
+    });
+
+    it('should hide ErrorDialog upon calling onClose cb', () => {
+      const context = shallow(<SearchBox />);
+      context.setState({
+        error: {},
+      });
+      context.find('ErrorDialog').attr('handleClose')();
+      context.rerender();
+
+      expect(context.find(<ErrorDialog />).length).to.equal(0);
+    });
   });
 
-  it('should change route upon address selection', () => {
-    jest.mock('preact-router');
+  describe('AlgoliaPlaces', () => {
+    it('should contain AlgoliaPlaces component', () => {
+      const apiKey = 'api-123';
+      const appId = 'app-no-1';
 
-    const context = shallow(<SearchBox onSelectAddress={sinon.spy()} />);
-    const algoliaPlaces = context.find('AlgoliaPlaces');
-    const lat = 14.53;
-    const lon = 15.54;
-    algoliaPlaces.output().attributes.onChange({ suggestion: { latlng: { lat, lng: lon } }});
+      process.env.ALGOLIA_API_KEY = apiKey;
+      process.env.ALGOLIA_APP_ID = appId;
 
-    expect(route).to.have.been.calledOnce;
+      expect(<SearchBox />).to.contain(<AlgoliaPlaces
+        placeholder="Var vill du söka från?"
+        options={{
+          language: 'sv',
+          countries: ['se'],
+          apiKey,
+          appId,
+        }}
+      />);
+    });
 
-    const args = route.args[0][0];
+    it('should update state upon AlgoliaPlaces onLimit event', () => {
+      const context = shallow(<SearchBox />);
 
-    expect(args).to.match(/\/skyddsrum/);
-    expect(args).to.match(new RegExp(`lat=${lat}`));
-    expect(args).to.match(new RegExp(`lon=${lon}`));
+      try {
+        context.find('AlgoliaPlaces').attr('onLimit')();
+      } catch (e) {}
 
-    jest.unmock('preact-router');
+      expect(context.state('error')).to.eql({
+        title: 'Adressförslagen kunde inte hämtas',
+        desc: <div>Tjänsten är för tillfället överbelastad och kan därmed inte visa förslag utifrån din sökning. Vi rekommenderar att du istället besöker <a href="https://gisapp.msb.se/apps/kartportal/enkel-karta_skyddsrum/">MSB</a> för att hitta ditt närmaste skyddsrum.</div>,
+      });
+    });
+
+    it('should throw an error, to be catched by Raven, upon AlgoliaPlaces onLimit event', () => {
+      const context = shallow(<SearchBox />);
+
+      expect(context.find('AlgoliaPlaces').attr('onLimit')).to.throw(Error, /Rate limit/);
+    });
+
+    it('should update state upon AlgoliaPlaces onError event', () => {
+      const context = shallow(<SearchBox />);
+
+      try {
+        context.find('AlgoliaPlaces').attr('onError')();
+      } catch (e) {}
+
+      expect(context.state('error')).to.eql({
+        title: 'Addressförslagen kunde inte hämtas',
+        desc: <div>Tjänsten för att hämta addressförslag är för tillfället inte tillgänglig. Vi rekommenderar att du istället besöker <a href="https://gisapp.msb.se/apps/kartportal/enkel-karta_skyddsrum/">MSB</a> för att hitta ditt närmaste skyddsrum.</div>,
+      });
+    });
+
+    it('should throw an error, to be catched by Raven, upon AlgoliaPlaces onError event', () => {
+      const context = shallow(<SearchBox />);
+      const error = new Error('This is bad');
+
+      // eslint-disable-next-line max-nested-callbacks
+      expect(() => context.find('AlgoliaPlaces').attr('onError')(error)).to.throw(Error, /This is bad/);
+    });
+
+    it('should change route upon address selection', () => {
+      jest.mock('preact-router');
+
+      const context = shallow(<SearchBox onSelectAddress={sinon.spy()} />);
+      const algoliaPlaces = context.find('AlgoliaPlaces');
+      const lat = 14.53;
+      const lon = 15.54;
+      algoliaPlaces.output().attributes.onChange({ suggestion: { latlng: { lat, lng: lon } }});
+
+      expect(route).to.have.been.calledOnce;
+
+      const args = route.args[0][0];
+
+      expect(args).to.match(/\/skyddsrum/);
+      expect(args).to.match(new RegExp(`lat=${lat}`));
+      expect(args).to.match(new RegExp(`lon=${lon}`));
+
+      jest.unmock('preact-router');
+    });
   });
 });
