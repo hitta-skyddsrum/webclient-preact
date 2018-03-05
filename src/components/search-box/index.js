413b43ddf2bd761070ea730633b93a73dbeb0f00
kontrollanten <alexander@width.se>
2018-03-05 13:43:03 +0100

    search-box: fix Algolia error handling

--- src/components/search-box/index.js
+++ src/components/search-box/index.js
@@ -3,28 +3,73 @@ import { autobind } from 'core-decorators';
 import { route } from 'preact-router';
 import classNames from 'classnames';
 import AlgoliacePlaces from 'algolia-places-react';
+import ErrorDialog from '../error-dialog';
 import styles from './style.scss';
 
 export default class SearchBox extends Component {
+  state = {
+    error: null,
+  };
+
   @autobind
   handleAddressSelection({ suggestion }) {
     route(`/skyddsrum?lat=${suggestion.latlng.lat}&lon=${suggestion.latlng.lng}`, false);
     this.props.onSelectAddress(suggestion);
   }
 
-  render() {
+  @autobind
+  handleAlgoliaLimit() {
+    this.setState({
+      error: {
+        title: 'Adressförslagen kunde inte hämtas',
+        desc: <div>Tjänsten är för tillfället överbelastad och kan därmed inte visa förslag utifrån din sökning. Vi rekommenderar att du istället besöker <a href="https://gisapp.msb.se/apps/kartportal/enkel-karta_skyddsrum/">MSB</a> för att hitta ditt närmaste skyddsrum.</div>,
+      },
+    });
+
+    throw new Error('AlgoliaPlaces: Rate limit');
+  }
+
+  @autobind
+  handleAlgoliaError(error) {
+    this.setState({
+      error: {
+        title: 'Addressförslagen kunde inte hämtas',
+        desc: <div>Tjänsten för att hämta addressförslag är för tillfället inte tillgänglig. Vi rekommenderar att du istället besöker <a href="https://gisapp.msb.se/apps/kartportal/enkel-karta_skyddsrum/">MSB</a> för att hitta ditt närmaste skyddsrum.</div>,
+      },
+    });
+
+    throw new Error(error);
+  }
+
+  @autobind
+  handleCloseErrorDialog() {
+    this.setState({ error: null });
+  }
+
+  render(props, { error }) {
     return (
-      <div className={classNames(styles.algolia, this.props.styles)}>
-        <AlgoliacePlaces
-          options={{
-            apiKey: process.env.ALGOLIA_API_KEY,
-            appId: process.env.ALGOLIA_APP_ID,
-            language: 'sv',
-            countries: ['se'],
-          }}
-          onChange={this.handleAddressSelection}
-          placeholder="Var vill du söka från?"
-        />
+      <div>
+        {error &&
+          <ErrorDialog
+            title={error.title}
+            desc={error.desc}
+            handleClose={this.handleCloseErrorDialog}
+          />
+        }
+        <div className={classNames(styles.algolia, props.styles)}>
+          <AlgoliacePlaces
+            options={{
+              apiKey: process.env.ALGOLIA_API_KEY,
+              appId: process.env.ALGOLIA_APP_ID,
+              language: 'sv',
+              countries: ['se'],
+            }}
+            onChange={this.handleAddressSelection}
+            onLimit={this.handleAlgoliaLimit}
+            onError={this.handleAlgoliaError}
+            placeholder="Var vill du söka från?"
+          />
+        </div>
       </div>
     );
   }
