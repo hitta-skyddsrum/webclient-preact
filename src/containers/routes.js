1cae0e8fd9c488c80594547e790a2864f71fe6f4
kontrollanten <alexander@width.se>
2018-03-10 08:21:20 +0100

    async routes: display loading indicator

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -2,6 +2,7 @@ import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 import AsyncRoute from 'preact-async-route';
 
+import LoadingIndicator from '../components/loading-indicator';
 import OmTjansten from '../components/om-tjansten';
 import VadArEttSkyddsrum from '../components/vad-ar-ett-skyddsrum';
 import NotFound from '../components/not-found';
@@ -19,9 +20,9 @@ export default class Routes extends Component {
   render() {
     return (
       <Router history={this.props.history}>
-        <AsyncRoute path="/" getComponent={loadHome} />
+        <AsyncRoute path="/" getComponent={loadHome} loading={() => <LoadingIndicator />} />
         <Redirect path="skyddsrum/koordinater/:lat/:lon" to="skyddsrum?lat=:lat&lon=:lon" />
-        <AsyncRoute path="skyddsrum/:id?" getComponent={loadShelters} />
+        <AsyncRoute path="skyddsrum/:id?" getComponent={loadShelters} loading={() => <LoadingIndicator />} />
         <VadArEttSkyddsrum path="vad-ar-ett-skyddsrum" />
         <OmTjansten path="om-tjansten" />
         <NotFound default />
