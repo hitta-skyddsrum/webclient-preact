ed4e9570c77e8ed18f8a608fcabc132339d61539
kontrollanten <alexander@width.se>
2018-03-11 21:24:51 +0100

    update e2e baseline and text regarding new data

--- src/components/om-tjansten/index.js
+++ src/components/om-tjansten/index.js
@@ -13,7 +13,7 @@ export default () => {
 
       <p>Hitta skyddsrum är en inofficiell webbtjänst baserad på <a href="https://www.msb.se/sv/Produkter--tjanster/Karttjanster/Inspiredirektivet/MSBs-Inspiretjanster/" target="_blank">data från MSB</a> . Databasen innehåller samtliga Sveriges skyddsrum.</p>
 
-      <p><em>Senaste data-import: 19 oktober 2016</em></p>
+      <p><em>Senaste data-import: 11 mars 2018</em></p>
 
       <p>Om du har synpunkter, frågor eller funderingar, kontakta oss på <a href="mailto:admin@hittaskyddsrum.se">admin@hittaskyddsrum.se</a>.</p>
     </ContentWrapper>
