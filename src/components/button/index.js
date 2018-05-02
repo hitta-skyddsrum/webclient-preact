d036261c5423946851cd7b0ea27d1b06b9f9a2d5
kontrollanten <alexander@width.se>
2018-05-02 23:32:31 +0200

    feat(button): add support for secondary styling

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,9 +2,12 @@ import { h } from 'preact';
 import Classnames from 'classnames/bind';
 import style from './style.scss';
 
-export default ({ children }) => {
+export default ({
+  children,
+  type = 'primary',
+}) => {
   const cx = Classnames.bind(style);
-  const className = cx(['button']);
+  const className = cx(['button', type]);
 
   return (
     <div class={className}>{children}</div>
