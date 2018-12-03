52d21637b6cbcef76e0d0146996e3c28454fc630
kontrollanten <alexander@width.se>
2018-12-03 23:26:31 +0100

    fix(analytics): hantera fel när google analytics inte är laddat
    fix #136

--- src/history.js
+++ src/history.js
@@ -3,7 +3,7 @@ import browserHistory from 'history/createBrowserHistory';
 const history = browserHistory();
 
 history.listen(location => {
-  gtag('config', GA_TRACKING_ID, {'page_path': location.pathname + location.search});
+  gtag && gtag('config', GA_TRACKING_ID, {'page_path': location.pathname + location.search});
 });
 
 export default history;
