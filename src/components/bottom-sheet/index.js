79a88811888f5a6fbb2531c90bbb91efda402e90
kontrollanten <alexander@width.se>
2019-02-23 00:40:43 +0100

    fix(BottomSheet): fixa stäng-ikonen

--- src/components/bottom-sheet/index.js
+++ src/components/bottom-sheet/index.js
@@ -1,8 +1,6 @@
 import { h } from 'preact';
 import Fab from 'preact-material-components/Fab';
 import 'preact-material-components/Fab/style.css';
-import Icon from 'preact-material-components/Icon';
-import 'preact-material-components/Icon/style.css';
 
 import styles from './styles.scss';
 
@@ -13,7 +11,9 @@ export default ({
 }) => (
   <div className={[styles.Container, isOpen && styles.IsOpen].filter(c => !!c).join(' ')}>
     <Fab className={`${styles.CloseButton} close`} onClick={onClose}>
-      <Icon>clear</Icon>
+      <div>
+        x
+      </div>
     </Fab>
     {children}
   </div>
